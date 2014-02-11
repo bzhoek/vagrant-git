04c5e6eee8f6356abaac5c30ca7c60b315abbd2f
Bas van der Hoek <bvdhoek@zilverline.com>
2014-02-11 20:00:21 +0100

    install git, make alice and bob staff, and create /opt/git writeable by staff

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,12 +3,24 @@ File { owner => 0, group => 0, mode => 0644 }
 
 user { "alice":
   ensure => present,
+  groups => 'staff',
   managehome => true,
   shell => '/bin/bash'
 }
 
 user { "bob":
   ensure => present,
+  groups => 'staff',
   managehome => true,
   shell => '/bin/bash'
+}
+
+package{ 'git':
+  ensure => 'present'
+}
+
+file{ ['/opt/git']:
+  ensure => 'directory',
+  group => 'staff',
+  mode => 770
 }
